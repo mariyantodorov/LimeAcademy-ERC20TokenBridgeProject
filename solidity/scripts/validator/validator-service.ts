a53f2d767ff3ab54fdb2f85bc7260fb185b6981c
Mariyan Todorov <mariyan.todorov13@gmail.com>
2022-11-24 16:26:57 +0200

    modified validator

--- solidity/scripts/validator/validator-service.ts
+++ solidity/scripts/validator/validator-service.ts
@@ -1,53 +1,41 @@
-const Web3 = require("web3")
-import { ethers } from "ethers"
-import hre from "hardhat"
-import { JsonRpcProvider } from "@ethersproject/providers"
-import { Contract } from "@ethersproject/contracts"
 import Bridge from "../../artifacts/contracts/Bridge.sol/Bridge.json"
 import ERC20 from "../../artifacts/contracts/ERC20Token.sol/ERC20Token.json"
 
-let bridges = new Map<number, Contract>()
-let providers = new Map<number, JsonRpcProvider>()
-const chainIdA = 11155111 //sepolia
-const chainIdB = 97 //Bsc
-
-//wss://sepolia.infura.io/ws/v3/199e48875a4f431e8ebaed6412865604
-//"https://data-seed-prebsc-1-s1.binance.org:8545"
-//"https://sepolia.infura.io/v3/199e48875a4f431e8ebaed6412865604"
-const providerChainA = new hre.ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:8545/"
-)
-const providerChainB = new hre.ethers.providers.JsonRpcProvider(
-    "HTTP://127.0.0.1:7545"
-)
-providers.set(chainIdA, providerChainA)
-providers.set(chainIdB, providerChainB)
-
+const Web3 = require("web3")
+let bridges = new Map<number, any>()
+let providers = new Map<number, any>()
+const chainIdA = 31337 //11155111 //sepolia
+const chainIdB = 1337 //97 //Bsc
 const adminPrivKey =
     "8b81e9caa6bf9213794c34c4a12b2fe85996a94bd4e0845659ba7325ddd3cdfc"
 const bridgeContractAddressChainA = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-const bridgeContractAddressChainB = ""
+const bridgeContractAddressChainB = "0xfA2f40104aBb08E89B762FDac5a16D03372b14c3"
 
-const bridgeContractChainA: Contract = new hre.ethers.Contract(
-    bridgeContractAddressChainA,
+const providerChainA = new Web3("http://127.0.0.1:8545/")
+providers.set(chainIdA, providerChainA)
+
+const providerChainB = new Web3("HTTP://127.0.0.1:7545")
+providers.set(chainIdB, providerChainB)
+
+const bridgeContractChainA = new providerChainA.eth.Contract(
     Bridge.abi,
-    providerChainA
+    bridgeContractAddressChainA
 )
 bridges.set(chainIdA, bridgeContractChainA)
 
-const bridgeContractChainB: Contract = new hre.ethers.Contract(
-    bridgeContractAddressChainB,
+const bridgeContractChainB = new providerChainB.eth.Contract(
     Bridge.abi,
-    providerChainB
+    bridgeContractAddressChainB
 )
-bridges.set(chainIdA, bridgeContractChainA)
+bridges.set(chainIdB, bridgeContractChainB)
 
 async function main() {
-    // bridgeEth.events.Lock(
-    //   {fromBlock: 0, step: 0}
-    // )
-    // .on('data', async event => handleLockEvent(event));
-    providerChainA.on(bridgeContractChainA.filters.Lock(), handleLockEvent)
+    console.log("start")
+    bridgeContractChainA.events
+        .Lock({ fromBlock: 0, step: 0 })
+        .on("data", async (event: any) => handleLockEvent(event))
+    console.log("end")
+    //providerChainA.on(bridgeContractChainA.filters.Lock(), handleLockEvent)
 }
 
 const handleLockEvent = async (lockEvent: any) => {
